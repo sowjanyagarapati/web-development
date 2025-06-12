d5e107e1646c8fe21826027dee2a738914208afe
sowjanyagarapati <lakshmisowjanya.garapati@gmail.com>
2025-06-12 22:23:34 +0530

    added sample counter

---
+++ src/designs/Counter.vue
@@ -0,0 +1,30 @@
+<template>
+    <button class="counter" @click="increment">
+            Count me {{ count }}
+    </button>
+</template>
+
+<script setup>
+
+import {ref} from 'vue';
+
+const count = ref(0);
+
+function increment() {
+    count.value++;
+    console.log(`Count is now: ${count.value}`);
+}
+</script>
+
+<style scoped>
+.counter {
+    margin: 5%;
+    font-size: small;
+    width: 150px;
+    height: 50px;
+    background: linear-gradient(to right, #4caf50, #81c784);
+}
+.counter:hover {
+    background: linear-gradient(to right, #052207, #a5d6a7);
+}
+</style>
--- src/views/ShowCase.vue
+++ src/views/ShowCase.vue
@@ -1,21 +1,44 @@
 <template>
-  <div class="grid">
-    <HelloWorld />
-    <Card />
-    <!-- Add more components here -->
+  <div class="wrapper">
+    <div class="grid">
+      <HelloWorld />
+      <Card />
+      <Counter />
+      <!-- Add more components here -->
+    </div>
   </div>
 </template>
 
 <script setup>
 import HelloWorld from '../designs/HelloWorld.vue'
 import Card from '../designs/Card.vue'
+import Counter from '../designs/Counter.vue'
 // Add more designs here as you create them
 </script>
 
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+  height: 100%;
+  width: 100%;
+  overflow: hidden; /* Prevent scrolling */
+}
+</style>
+
 <style scoped>
 .grid {
+  padding: 5rem;
   display: flex;
-  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  /* grid-template-columns: repeat(auto-fit, minmax(300px, 1fr)); */
   gap: 2rem;
+}
+.wrapper {
+  background: linear-gradient(135deg, #ff7eb3, #ff758c, #ff6a5c);
+  background-size: cover;
+  height: 100vh; /* Full viewport height */
+  width: 100vw;
+  margin: 0;
+  padding: 0;
 }
 </style>
